966430e51c585520994302f7c5ea1c1b7195377c
PUCKCODERS <jonitaweb.jobs@gmail.com>
2025-04-06 00:59:32 -0500

    imagenes

--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -10,18 +10,40 @@ import { MdOutlineZoomOutMap } from "react-icons/md";
 const ProductItem = () => {
   return (
     <div className="productItem shadow-[8px_7px_12px_#b8b8b8] rounded-md  overflow-hidden !border-1 !border-[#b1cdee] mb-4">
-      <div className="imgWrapper w-[100%] h-[200px] overflow-hidden rounded-md relative">
-        <img
-          src="https://polipapel.vteximg.com.br/arquivos/ids/174453-1000-1000/O76237.png?v=638191796905070000"
-          className=" w-full"
-        />
+      <div className=" group imgWrapper w-[100%]  overflow-hidden rounded-md relative">
+        <Link to="/">
+          <div className="img h-[200px] overflow-hidden">
+            <img
+              src="https://scontent.fuio22-1.fna.fbcdn.net/v/t39.30808-6/487313297_4163892343898410_8483451465957424973_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=ZVwNjnmxeAQQ7kNvwERUBNY&_nc_oc=AdlGVrYk-Oa5k_c9M72I6asS9xyuiCukCNWOEZY-QQ7ZpMbxUR3NRXPRDM77NFv81nw&_nc_zt=23&_nc_ht=scontent.fuio22-1.fna&_nc_gid=BJXewWJttIXCbVd2DUfPCw&oh=00_AfGMszGvDGtuq44o5hT9O6zFe3aWH4YtaffUnNQcxz8amA&oe=67F7F8EA"
+              className=" w-full"
+            />
+
+            <img
+              src="https://scontent.fuio22-1.fna.fbcdn.net/v/t39.30808-6/487769993_4164970857123892_2742528583091687138_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_ohc=0_OW6dMCqs4Q7kNvwFkaVI2&_nc_oc=AdlYfPXXX8Sjl0QZ6DikgK2W_0WunjUR19HcBm85tpbLklVuRDk3Fqysezw25Pk-NrI&_nc_zt=23&_nc_ht=scontent.fuio22-1.fna&_nc_gid=yrW--Gp5F6HjRRr62WPSMg&oh=00_AfFhiHpqA2NJ8i2SzCXMFEkotAiI-2u6QyeA0P88Q5yl2Q&oe=67F7D415"
+              className=" w-full"
+            />
+
+            <img
+              src="https://scontent.fuio22-1.fna.fbcdn.net/v/t39.30808-6/487392436_4163905387230439_619907725970957130_n.png?_nc_cat=105&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=cqyzFiZaX_sQ7kNvwFhNZkr&_nc_oc=AdmiRHtb49Nfd_CcxldwCHvzYkOr2gqXPTaVqfy0vqJa0_KAfdo_S23KDjyGvHY0I1Q&_nc_zt=23&_nc_ht=scontent.fuio22-1.fna&_nc_gid=rAjK0BzDm7z6kLbjTIqokQ&oh=00_AfFIRtNZACyow4xnVLzjqgRtQDkjtPFHL7eQcxVJo94Nzw&oe=67F7D7A7"
+              className=" w-full"
+            />
+          </div>
+        </Link>
         <span className="discount flex items-center absolute top-[10px] left-[10px] z-50 bg-[#556f8d] text-white h-[20px] p-1 text-[12px] font-[500] rounded-lg">
           10%
         </span>
 
-        <div className="actions absolute top-[15px] right-[15px] z-50 flex items-center gap-4 flex-col w-[80px]">
-          <Button className="!w-[45px] !h-[45px] !min-w-[45px] rounded-full bg-![#fd0505] !text-[#f88787] !hover:bg-[#fd0505] !hover:text-[#fd0505] ">
-            <FaHeart />
+        <div className="actions absolute top-[-200px] right-[5px] z-50 flex items-center gap-2 flex-col w-[50px] transition-all duration-300 group-hover:top-[15px] opacity-0 group-hover:opacity-100">
+          <Button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full !bg-white !text-[#f58686]  hover:!bg-[#fdaeae] hover:!text-[#f10606] group ">
+            <FaHeart className="text-[18px] !text-[#f58686] group-hover:!text-[#f10606] hover:!text-[#f10606]" />
+          </Button>
+
+          <Button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full !bg-white !text-[#b1cdee]  hover:!bg-[#b1cdee] hover:!text-[#556f8d] group ">
+            <IoMdGitCompare className="text-[18px] !text-[#b1cdee] group-hover:!text-[#556f8d] hover:!text-[#556f8d]" />
+          </Button>
+
+          <Button className="!w-[35px] !h-[35px] !min-w-[35px] !rounded-full !bg-white !text-[#b1cdee]  hover:!bg-[#b1cdee] hover:!text-[#556f8d] group ">
+            <MdOutlineZoomOutMap className="text-[18px] !text-[#b1cdee] group-hover:!text-[#556f8d] hover:!text-[#556f8d]" />
           </Button>
         </div>
       </div>
